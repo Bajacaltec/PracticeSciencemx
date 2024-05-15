ba48256aeca5f94d9a4bd416bc86b59aa1dd546c
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2024-05-15 14:40:01 +0200

    a
    a

---
+++ pages/1 Pensamiento crítico.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+import pandas as pd 
+from streamlit_option_menu import option_menu
+
+
+st.sidebar.caption('By Baja Caltec')
+
+Desactivar=False
+#from streamlit_lottie import st_lottie
+contra = st.sidebar.text_input('Contraseña', type='password')
+falcho=False
+ingresar=st.sidebar.toggle('Ingresar',disabled=Desactivar)
+
+st.sidebar.caption('El conteo de 10 minutos inicia al hacer click en ingresar')
+usuarios = ('2089', '234')
+
+if ingresar ==True and contra in usuarios:
+    st.title('Pensamiento crítico') 
+    tab1,tab2,tab3=st.tabs(['Pensamiento crítico','Historía','Tipos de ciencia'])
+    with tab1:
+        st.title('¿Qué es el pensamiento crítico?')
