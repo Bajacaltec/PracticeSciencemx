31b75b65bb68596488c8e56951e108d723a8d72a
mdfnunez <65826102+mdfnunez@users.noreply.github.com>
2024-05-25 14:16:28 +0200

    a

--- pages/Evaluación 2.py
+++ pages/Evaluación 2.py
@@ -3,13 +3,3 @@ import psycopg2
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from streamlit_gsheets import GSheetsConnection
-
-conn = st.connection("gsheets", type=GSheetsConnection)
-
-df = conn.read()
-
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
-#Base de datos
--- pages/Evaluación 3.py
+++ pages/Evaluación 3.py
@@ -4,28 +4,3 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-#Base de datos
-conn = psycopg2.connect(
-                        host="dpg-cokdc7gl5elc73c3klp0-a.oregon-postgres.render.com",
-                        database="bajacaltec_ciencia",
-                        user="bajacaltec_ciencia_user",
-                        password="QnVGnpcQGxEr7q9W3YDiPS4ABxSTkAVn"
-                    )
-
-st.sidebar.caption('By Baja Caltec')
-
-Desactivar=False
-#from streamlit_lottie import st_lottie
-contra = st.sidebar.text_input('Contraseña', type='password')
-falcho=False
-ingresar=st.sidebar.toggle('Ingresar',disabled=Desactivar)
-
-usuarios = ('2089', '234')
-
-if ingresar ==True and contra in usuarios:
-        st.title('Anteproyecto')
-
-        tab1,tab2,tab3,tab4=st.tabs(['Idea de investigación','Introducción','Planteamiento del problema','Justificación'])
-        with tab1:
-                st.empty()
